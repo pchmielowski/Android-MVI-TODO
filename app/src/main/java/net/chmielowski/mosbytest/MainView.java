129c4846b2fd5fc6e56e47a5e62b415c05938008
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-02-20 12:54:10 +0100

    NonNull

--- app/src/main/java/net/chmielowski/mosbytest/MainView.java
+++ app/src/main/java/net/chmielowski/mosbytest/MainView.java
@@ -1,10 +1,13 @@
 package net.chmielowski.mosbytest;
 
+import android.support.annotation.NonNull;
+
 import com.hannesdorfmann.mosby3.mvp.MvpView;
 
 import io.reactivex.Observable;
 
 interface MainView extends MvpView {
+    @NonNull
     Observable<String> queryChanged();
 
     void render(MainViewState viewState);
