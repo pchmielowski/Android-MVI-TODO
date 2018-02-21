49c8b5f8584f2f5967038b16455a8cb43b9f3899
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-02-21 11:51:04 +0100

    MviBasePresenterHelper

--- main/src/main/java/net/chmielowski/todo/main/MainPresenter.java
+++ main/src/main/java/net/chmielowski/todo/main/MainPresenter.java
@@ -1,15 +1,30 @@
 package net.chmielowski.todo.main;
 
+import android.support.annotation.NonNull;
+
 import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
 import com.hannesdorfmann.mosby3.mvi.MviBasePresenter;
+import com.hannesdorfmann.mosby3.mvp.MvpView;
 
 import java.util.function.Function;
 
 import io.reactivex.Observable;
 
+abstract class MviBasePresenterHelper<V extends MvpView, VS> extends MviBasePresenter<V, VS> {
+    @Override
+    final protected void bindIntents() {
+        subscribeViewState(intentStream(), this::render);
+    }
+
+    abstract void render(final V view, final VS viewState);
+
+    @NonNull
+    abstract Observable<VS> intentStream();
+}
+
 @AutoFactory
-public final class MainPresenter extends MviBasePresenter<MainView, MainViewState> {
+final class MainPresenter extends MviBasePresenterHelper<MainView, MainViewState> {
     private final Delegate delegate;
 
     MainPresenter(@Provided Delegate delegate) {
@@ -20,11 +35,15 @@ public final class MainPresenter extends MviBasePresenter<MainView, MainViewStat
         return super.intent(view -> function.apply(view.intents()));
     }
 
+    @NonNull
     @Override
-    protected void bindIntents() {
-        final Observable<MainViewState> merge = delegate.createStream(intent(MainView.Intents::textChanged), intent(MainView.Intents::addNewClicked), this.intent(MainView.Intents::textChanged));
+    Observable<MainViewState> intentStream() {
+        return delegate.createStream(intent(MainView.Intents::textChanged), intent(MainView.Intents::addNewClicked), this.intent(MainView.Intents::textChanged));
+    }
 
-        subscribeViewState(merge, (view, viewState) -> view.renderer().render(viewState));
+    @Override
+    void render(final MainView view, final MainViewState viewState) {
+        view.renderer().render(viewState);
     }
 
 }
